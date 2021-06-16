3cbdc0d8754f41f037855589bb8d503a368fca53
Retro_Guy <retro.guy@rocksolidbbs.com>
2021-06-16 09:04:22 +0000

    Cleanup creating links and add protocols

--- Rocksolid_Light/rocksolid/newsportal.php
+++ Rocksolid_Light/rocksolid/newsportal.php
@@ -883,11 +883,11 @@ function html_parse($text) {
   $n=count($words);
   for($i=0; $i<$n; $i++) {
     $word=$words[$i];
-    if(preg_match('/https?\:\/\/[^\",]+/i', $word)) {
-      $nlink = preg_replace('/(\&lt|\&gt);/', '', $word);
-      $nlink = rtrim($nlink, '.');
+    if(preg_match('/(https?|ftp|news|gopher|telnet)\:\/\/[^\",]+/i', $word)) {
+      $nlink = trim($word, '().');
+      $nlink = preg_replace('/(\&lt|\&gt|\&nbsp);/', '', $nlink);
       $nword = '<a '.$target.' href="'.$nlink.'">'.$word.'</a>';
-      if($nword!=$word) {
+      if($nword!=$word && substr($nlink, strlen($nlink) - 3) != "://") { 
         $word=$nword;
       }
     }
